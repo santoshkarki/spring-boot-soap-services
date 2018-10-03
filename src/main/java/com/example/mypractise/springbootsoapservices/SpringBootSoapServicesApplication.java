fab0e407b0095ffe3d228398dcb1388f194f990c
santosh <santosh.kaarki@gmail.com>
2018-10-03 09:00:10 -0500

    Initial Project Setup

---
+++ src/main/java/com/example/mypractise/springbootsoapservices/SpringBootSoapServicesApplication.java
@@ -0,0 +1,12 @@
+package com.example.mypractise.springbootsoapservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSoapServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSoapServicesApplication.class, args);
+	}
+}
